c04bb683c62cab2adffdc4bc8d4718cb8c7adf81
Ahmad Yazid Munif <ahmadyazid76@gmail.com>
2024-12-04 17:18:25 +0700

    feat: add allowed url cors

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,4 +1,4 @@
-// daftar url yang dapat melakukan request ke backend
-const allowedOrigin = [
-    "http://localhost:3000","http://google.com"];
-module.exports = allowedOrigin
+// whitelist --> url yang dapat mengakses/me-request ke backend
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+
+module.exports = allowedOrigins;
