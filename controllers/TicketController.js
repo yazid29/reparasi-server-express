4b1f0fecbb57b7cbe3b05b345c0fa809a3350794
Ahmad Yazid Munif <ahmadyazid76@gmail.com>
2024-12-07 19:05:43 +0700

    fix: CRUD ticket

--- controllers/TicketController.js
+++ controllers/TicketController.js
@@ -3,72 +3,101 @@ const Ticket = require("../models/Ticket");
 const asyncHandler = require("express-async-handler");
 
 const getAllTickets = asyncHandler(async (req, res) => {
-    const tickets = await Ticket.find().lean().exec();
-    if(!tickets?.length) {
-        return res.status(404).json({message: "No tickets found"});
-    }
-    res.status(200).json(tickets);
-});
+  // query untuk ambil data ticket
+  const tickets = await Ticket.find().lean();
 
+  // validasi jika tidak ada user
+  if (!tickets?.length) {
+    return res.status(400).json({ message: "No tickets found" });
+  }
+
+  const ticketsWithDataUser = await Promise.all(
+    tickets.map(async (ticket) => {
+      const user = await User.findById(ticket.user).lean().exec();
+
+      return { ...ticket, username: user.username };
+    })
+  );
+
+  res.status(200).json({
+    data: ticketsWithDataUser,
+    message: "Success get all tickets",
+  });
+});
+const getByIdTicket = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  // query untuk ambil data ticket
+  const tickets = await Ticket.findById(id).lean().exec();
+
+  // validasi jika tidak ada user
+  if (!tickets) {
+    return res.status(400).json({ message: "No tickets found" });
+  }
+
+  const user = await User.findById(tickets.user).lean().exec();
+  tickets.username = user.username;
+  res.status(200).json({
+    data: tickets,
+    message: "Success get all tickets",
+  });
+});
 const createTicket = asyncHandler(async (req, res) => {
-    const {title,desc,user} = req.body;
-    if(!title || !desc || !user) {
-        return res.status(400).json({message: "All fields are required"});
-    }
-    const userExists = await User.findById(user).exec();
-    if(!userExists) {
-        return res.status(400).json({message: "User not found"});
-    }
-    const ticket = await Ticket.create({title,desc,user});
-    if(ticket) {
-        res.status(201).json({message: "New ticket created"});
-    } else{
-        res.status(400).json({message: "Failed to create new ticket"});
-    }
+  const { title, desc, user } = req.body;
+  if (!title || !desc || !user) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const userExists = await User.findById(user).exec();
+  if (!userExists) {
+    return res.status(400).json({ message: "User not found" });
+  }
+  const ticket = await Ticket.create({ title, desc, user });
+  if (ticket) {
+    res.status(201).json({ message: "New ticket created" });
+  } else {
+    res.status(400).json({ message: "Failed to create new ticket" });
+  }
 });
 
 const updateTicket = asyncHandler(async (req, res) => {
-    const {id, title,desc, isFixed, user} = req.body;
-    if(!id || 
-        !title || 
-        !desc || 
-        typeof isFixed !== "boolean" || 
-        !user) {
-        return res.status(400).json({message: "All fields are required"});
-    }
-    const ticket = await Ticket.findById(id).exec();
-    if(!ticket) {
-        return res.status(400).json({message: "Ticket not found"});
-    }
-    const userExists = await User.findById(user).exec();
-    if(!userExists) {
-        return res.status(400).json({message: "User not found"});
-    }
-    ticket.title = title;
-    ticket.desc = desc;
-    ticket.isFixed = isFixed;
-    ticket.user = user;
-    const updatedTicket = await ticket.save();
-    res.json({message: `${id} updated`});
+  const { id, title, desc, isFixed, user, ticket } = req.body;
+  if (!id || !title || !desc || !user) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const ticketData = await Ticket.findById(id).exec();
+  if (!ticketData) {
+    return res.status(400).json({ message: "Ticket not found" });
+  }
+  const userExists = await User.findById(user).exec();
+  if (!userExists) {
+    return res.status(400).json({ message: "User not found" });
+  }
+  ticketData.title = title;
+  ticketData.desc = desc;
+  ticketData.isFixed = isFixed == "true" ? true : false;
+  ticketData.user = user;
+  ticketData.ticket = ticket;
+  const updatedTicket = await ticketData.save();
+  res.json({ message: `${id} updated` });
 });
 
 const deleteTicket = asyncHandler(async (req, res) => {
-    const {id} = req.body;
-    if(!id) {
-        return res.status(400).json({message: "Ticket ID required"});
-    }
-    const ticket = await Ticket.findById(id).exec();
-    if(!ticket) {
-        return res.status(404).json({message: "Ticket not found"});
-    }
-    const result = await ticket.deleteOne();
-    const reply = `Ticket with ID ${id} deleted`;
-    res.json(reply);
+  const { id } = req.body;
+  if (!id) {
+    return res.status(400).json({ message: "Ticket ID required" });
+  }
+  const ticket = await Ticket.findById(id).exec();
+  if (!ticket) {
+    return res.status(404).json({ message: "Ticket not found" });
+  }
+  const result = await ticket.deleteOne();
+  const reply = `Ticket with ID ${id} deleted`;
+  res.json(reply);
 });
 
 module.exports = {
-    getAllTickets,
-    createTicket,
-    updateTicket,
-    deleteTicket
+  getAllTickets,
+  createTicket,
+  updateTicket,
+  deleteTicket,
+  getByIdTicket,
 };
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const ticketsController = require("../controllers/TicketController");
 const JWTVerifier = require("../middleware/JWTVerifier");
 
-router.use(JWTVerifier);
+// router.use(JWTVerifier);
 router.route("/")
     .get(ticketsController.getAllTickets)
     .post(ticketsController.createTicket)
     .patch(ticketsController.updateTicket)
     .delete(ticketsController.deleteTicket);
-
+router.route("/:id").get(ticketsController.getByIdTicket);
 module.exports = router;
